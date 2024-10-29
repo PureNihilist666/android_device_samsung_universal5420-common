8c0a1a721ce56d56853c35ee5c7d2177cd7c999d
PureNihilist666 <pranav.nath@outlook.com>
2024-10-29 18:20:34 +0530

    universal5420-common: Added missing libcutils dependency

--- libshims/libshim_atomic/Android.mk
+++ libshims/libshim_atomic/Android.mk
@@ -2,6 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := atomic.cpp
+LOCAL_SHARED_LIBRARIES := libcutils
 LOCAL_MODULE := libshim_atomic
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
